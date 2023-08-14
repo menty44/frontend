490e030c1b182d342d52052bb768dd19a59fec22
Stanley-rgb <briannjogu42@gmail.com>
2023-08-14 21:38:33 +0300

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -33,14 +33,52 @@ export default function HomePage() {
                 <div>
                     <div className="sm:mx-auto sm:w-full sm:max-w-sm">
 
-                        <h2 style={{ fontFamily: 'Ubuntu, sans-serif' }} className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
-                            Register
+                        <h2 style={{ fontFamily: 'Ubuntu, sans-serif' }} className="mt-10 text-left text-2xl font-bold leading-9 tracking-tight text-white">
+                            Sign Up
                         </h2>
                     </div>
 
-                    <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <div className="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
                         <form className="space-y-6" action="#" method="POST">
+                        <div className="mt-2 flex gap-2">
+                        <div>
+                                <label htmlFor="username" className="block text-sm font-medium leading-6 text-white">
+                                    First name
+                                </label>
+                            <input
+                                id="firstName"
+                                name="firstName"
+                                type="text"
+                                required
+                                className="w-full rounded-full border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                            />
+                            </div>
                             <div>
+                                <label htmlFor="username" className="block text-sm font-medium leading-6 text-white">
+                                    Middle name
+                                </label>
+                            <input
+                                id="lastName"
+                                name="lastName"
+                                type="text"
+                                required
+                                className="w-full rounded-full border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                            />
+                            </div><div>
+                                <label htmlFor="username" className="block text-sm font-medium leading-6 text-white">
+                                    Surname
+                                </label>
+                            <input
+                                id="surname"
+                                name="surname"
+                                type="text"
+                                required
+                                className="w-full rounded-full border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                            />
+                            </div>
+                        </div>
+
+                            {/* <div>
                                 <label htmlFor="username" className="block text-sm font-medium leading-6 text-white">
                                     UserName
                                 </label>
@@ -54,12 +92,13 @@ export default function HomePage() {
                                         style={{textAlign: 'center'}}
                                     />
                                 </div>
-                            </div>
+                            </div> */}
 
                             <div>
                                 <label htmlFor="email" className="block text-sm font-medium leading-6 text-white">
                                     Email Address
                                 </label>
+                                
                                 <div className="mt-2">
                                     <input
                                         id="username"
@@ -71,6 +110,76 @@ export default function HomePage() {
                                     />
                                 </div>
                             </div>
+                            {/* <div className="mt-2">
+                            <div className="flex gap-5 w-full">
+                                <div className="flex-1">
+                                    <label htmlFor="phone" className="block text-sm font-medium leading-6 text-white">
+                                        Phone
+                                    </label>
+                                    <input
+                                        id="phone"
+                                        name="phone"
+                                        type="text"
+                                        required
+                                        className="w-full rounded-full border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                                        placeholder="Placeholder"
+                                        style={{textAlign: 'center'}}
+                                    
+                                    />
+                                </div>
+                                <div className="flex-1">
+                                    <div className="mt-3">
+										<input
+											id="phoneNumber"
+											name="phoneNumber"
+											type="phoneNumber"
+											required
+											className="w-full rounded-full border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+											style={{textAlign: 'center'}}
+										/>
+									</div>
+                                </div>
+                            </div>
+                        </div> */}
+                            <div className="mt-2">
+                            <div className="flex gap-5 w-full">
+                                <div className="flex-1">
+                                    <label htmlFor="nationality" className="block text-sm font-medium leading-6 text-white">
+                                        Nationality
+                                    </label>
+                                    <input
+                                        id="nationality"
+                                        name="nationality"
+                                        type="text"
+                                        required
+                                        className="w-full rounded-full border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                                        placeholder="Placeholder"
+                                        style={{textAlign: 'center'}}
+                                    
+                                    />
+                                </div>
+                                <div className="flex-1">
+                                    <label htmlFor="gender" className="block text-sm font-medium leading-6 text-white">
+                                        Gender
+                                    </label>
+                                    <div className="mt-0">
+                                        <select
+                                            id="gender"
+                                            name="gender"
+                                            required
+                                            className="w-full rounded-full border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                                            style={{textAlign: 'center'}}
+                                       >
+                                            <option value="" disabled selected>Select your gender</option>
+                                            <option value="male">Male</option>
+                                            <option value="female">Female</option>
+                                            <option value="other">Other</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
 
                             <div>
                                 <div className="flex items-center justify-between">
@@ -88,6 +197,7 @@ export default function HomePage() {
                                         required
                                         className=" w-full rounded-full border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
                                         style={{textAlign: 'center'}}
+                                        placeholder="Enter password"
                                     />
                                 </div>
                             </div>
@@ -107,22 +217,35 @@ export default function HomePage() {
                                         type="password"
                                         required
                                         className="rounded-full w-full rounded-full border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                                        placeholder="Repeat password"
                                         style={{textAlign: 'center'}}
+                                        
                                   />
                                 </div>
                             </div>
-
+                            <div className="mt-2">
+                            <label htmlFor="terms" className="flex items-center">
+                            <input
+                                    id="terms"
+                                    name="terms"
+                                    type="checkbox"
+                                    required
+                                    className="rounded text-sky-600 focus:ring-0"
+                                />
+                                <span className="text-sm text-white ml-2">Agree to <a href="#" className="text-blue-300 underline">Terms and Conditions</a></span>
+                            </label>
+                            </div>
                             <div>
                                 <button
                                     type="submit"
                                     className="rounded-full flex w-full justify-center bg-sky-300 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600"
                                 >
-                                    Register
+                                    Continue
                                 </button>
                             </div>
                         </form>
 
-                        <p className="mt-10 text-center text-sm text-white">
+                        <p className="mt-1 text-center text-sm text-white">
                             Already have an account?{' '}
                             <Link href="/login" className="font-semibold leading-6 text-sky-300 hover:text-sky-200">
                                 signin
