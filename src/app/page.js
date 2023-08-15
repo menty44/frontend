2511e576cfea95d19acf16a7fcf571a0695b81af
Stanley-rgb <briannjogu42@gmail.com>
2023-08-15 16:16:45 +0300

    changed

--- src/app/page.js
+++ src/app/page.js
@@ -38,8 +38,8 @@ export default function HomePage() {
                         </h2>
                     </div>
 
-                    <div className="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
-                        <form className="space-y-6" action="#" method="POST">
+                    <div className="mt-3 sm:mx-auto sm:w-full sm:max-w-sm">
+                        <form className="space-y-3" action="#" method="POST">
                         <div className="mt-2 flex gap-2">
                         <div>
                                 <label htmlFor="username" className="block text-sm font-medium leading-6 text-white">
@@ -110,7 +110,7 @@ export default function HomePage() {
                                     />
                                 </div>
                             </div>
-                            {/* <div className="mt-2">
+                            <div className="mt-2">
                             <div className="flex gap-5 w-full">
                                 <div className="flex-1">
                                     <label htmlFor="phone" className="block text-sm font-medium leading-6 text-white">
@@ -122,13 +122,12 @@ export default function HomePage() {
                                         type="text"
                                         required
                                         className="w-full rounded-full border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
-                                        placeholder="Placeholder"
                                         style={{textAlign: 'center'}}
                                     
                                     />
                                 </div>
                                 <div className="flex-1">
-                                    <div className="mt-3">
+                                    <div className="mt-6">
 										<input
 											id="phoneNumber"
 											name="phoneNumber"
@@ -140,8 +139,8 @@ export default function HomePage() {
 									</div>
                                 </div>
                             </div>
-                        </div> */}
-                            <div className="mt-2">
+                        </div>
+                            <div className="mt-1">
                             <div className="flex gap-5 w-full">
                                 <div className="flex-1">
                                     <label htmlFor="nationality" className="block text-sm font-medium leading-6 text-white">
@@ -189,7 +188,7 @@ export default function HomePage() {
                                     </label>
 
                                 </div>
-                                <div className="mt-2">
+                                <div className="mt-1">
                                     <input
                                         id="password"
                                         name="password"
@@ -210,7 +209,7 @@ export default function HomePage() {
                                     </label>
 
                                 </div>
-                                <div className="mt-2">
+                                <div className="mt-1">
                                     <input
                                         id="confirmpassword"
                                         name="confirmpassword"
@@ -223,7 +222,7 @@ export default function HomePage() {
                                   />
                                 </div>
                             </div>
-                            <div className="mt-2">
+                            <div className="mt-1">
                             <label htmlFor="terms" className="flex items-center">
                             <input
                                     id="terms"
